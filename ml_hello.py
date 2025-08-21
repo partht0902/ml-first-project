8b2851029e53b37e0bca4d056c17836f8042665d
partht0902 <parthtendulkar55@gmail.com>
2025-08-22 00:18:14 +0530

    Update ml_hello.py

--- ml_hello.py
+++ ml_hello.py
@@ -1,8 +1,1 @@
-from sklearn.datasets import load_iris 
-from sklearn.model_selection import train_test_split 
-from sklearn.linear_model import LogisticRegression 
-data = load_iris() 
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42) 
-model = LogisticRegression(max_iter=200) 
-model.fit(X_train, y_train) 
-print("Accuracy:", model.score(X_test, y_test)) 
+
